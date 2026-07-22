9ef3f0af37f68c19395234d63b8d14449ed348a3
Atharv_1204 <atharvpal1204@gmail.com>
2026-07-23 00:07:01 +0530

    Time: 6 ms (43.29%), Space: 44.5 MB (67.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // If lengths differ, they can't be anagrams
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // Create a frequency array for 26 lowercase letters
+        int[] count = new int[26];
+
+        // Count characters in s and subtract for t
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        // Check if all counts are zero
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
