b62257f163910c079f4149a36a7ea8198e7e1761
Atharv_1204 <atharvpal1204@gmail.com>
2026-07-28 01:21:32 +0530

    Time: 2 ms (52.75%), Space: 43.9 MB (77.68%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,16 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int missingInteger(int[] nums) {
+        // Step 1: Calculate sum of the longest consecutive prefix
         int countSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] + 1 == nums[i]) countSum += nums[i];
-            else break;
-        }
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (countSum == nums[i]) {
-                countSum++;
+            if (nums[i] == nums[i - 1] + 1) {
+                countSum += nums[i];
+            } else {
+                break;
             }
         }
+
+        // Step 2: Use a HashSet for O(1) lookups instead of sorting
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        // Step 3: Increment countSum until it's not in the set
+        while (set.contains(countSum)) {
+            countSum++;
+        }
+
         return countSum;
     }
 }
