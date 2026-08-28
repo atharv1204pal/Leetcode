5ce839d32bccf5d018fdc3e9320dfd661884181b
Atharv_1204 <atharvpal1204@gmail.com>
2026-08-28 21:27:28 +0530

    Time: 0 ms (100%), Space: 44.8 MB (16.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,26 +2,18 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int i = n - 2;
-        
-        // Step 1: Find the pivot
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-        
         if (i >= 0) {
-            // Step 2: Find the successor
             int j = n - 1;
             while (nums[j] <= nums[i]) {
                 j--;
             }
-            // Step 3: Swap pivot and successor
             swap(nums, i, j);
         }
-        
-        // Step 4: Reverse the suffix
         reverse(nums, i + 1, n - 1);
     }
-    
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
